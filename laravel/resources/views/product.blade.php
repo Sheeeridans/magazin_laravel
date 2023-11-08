c1bf26386190c15fc999618f163677921da05ded
sheridans <asy201196@yandex.ru>
2023-11-08 14:49:23 +0300

    less 2

--- laravel/resources/views/product.blade.php
+++ laravel/resources/views/product.blade.php
@@ -1,4 +1,4 @@
-!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -34,10 +34,10 @@
     </div>
 </nav>
 
-
 <div class="container">
     <div class="starter-template">
         <h1>iPhone X 64GB</h1>
+        <h2>{{ $product }}</h2>
         <p>Цена: <b>71990 руб.</b></p>
         <img src="http://laravel-diplom-1.rdavydov.ru/storage/products/iphone_x.jpg">
         <p>Отличный продвинутый телефон с памятью на 64 gb</p>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,14 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [\App\Http\Controllers\MainController::class, 'index']);
+Route::get('/categories', [\App\Http\Controllers\MainController::class, 'categories']);
+Route::get('/mobiles/{product}', [\App\Http\Controllers\MainController::class, 'product']);
+Route::get('/{category}', [\App\Http\Controllers\MainController::class, 'category']);
 
-Route::get('/categories', function () {
-    return view('categories');
-});
-
-Route::get('/mobiles/iphone_x_64', function () {
-    return view('product');
-});
