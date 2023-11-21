d504e5b40f5751770c470046203052574eb65c6f
sheridans <asy201196@yandex.ru>
2023-11-21 15:37:13 +0300

    less 11 Создание Middleware, Auth

--- laravel/app/Http/Controllers/BasketController.php
+++ laravel/app/Http/Controllers/BasketController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BasketController extends Controller
 {
@@ -60,6 +61,12 @@ class BasketController extends Controller
         } else {
             $order->products()->attach($productId);
         }
+
+        if (Auth::check()) {
+            $order->user_id = Auth::id();
+            $order->save();
+        }
+
         $product = Product::find($productId);
         session()->flash('success', 'Добавлен товар ' . $product->name);
         return redirect()->route('basket');
