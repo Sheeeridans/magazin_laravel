fc777e7f7a4120e5b89b7dc175bfcf38b6ac85f5
sheridans <asy201196@yandex.ru>
2023-11-22 16:02:39 +0300

    less 18 Pagination, QueryBuilder, Фильтры

--- laravel/app/Http/Controllers/MainController.php
+++ laravel/app/Http/Controllers/MainController.php
@@ -5,25 +5,47 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Http\Requests\ProductsFilterRequest;
 
 class MainController extends Controller
 {
-    public function index() {
-        $products = Product::get();
+    public function index(ProductsFilterRequest $request)
+    {
+        $productsQuery = Product::query();
+
+        if ($request->filled('price_from')) {
+            $productsQuery->where('price', '>=', $request->price_from);
+        }
+
+        if ($request->filled('price_to')) {
+            $productsQuery->where('price', '<=', $request->price_to);
+        }
+
+        foreach (['hit', 'new', 'recommend'] as $field) {
+            if ($request->has($field)) {
+                $productsQuery->where($field, 1);
+            }
+        }
+
+        $products = $productsQuery->paginate(6)->withPath("?" . $request->getQueryString());
+
         return view('index', compact('products'));
     }
 
-    public function categories() {
+    public function categories()
+    {
         $categories = Category::get();
         return view('categories', compact('categories'));
     }
 
-    public function category($code) {
+    public function category($code)
+    {
         $category = Category::where('code', $code)->first();
         return view('category', compact('category'));
     }
 
-    public function product($category, $product = null) {
+    public function product($category, $product = null)
+    {
         return view('product', ['product' => $product]);
     }
 }
