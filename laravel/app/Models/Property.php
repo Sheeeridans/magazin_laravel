dcb31864f6c46413c36877801064c6d160736b4d
sheridans <asy201196@yandex.ru>
2023-11-25 18:05:11 +0300

    less 32 Товарные предложения

---
+++ laravel/app/Models/Property.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\Translatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Property extends Model
+{
+    use SoftDeletes, Translatable;
+
+    protected $fillable = ['name', 'name_en'];
+
+    public function propertyOptions()
+    {
+        return $this->hasMany(PropertyOption::class);
+    }
+
+    //TODO: check table name and fields
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
+}
---
+++ laravel/app/Models/PropertyOption.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\Translatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PropertyOption extends Model
+{
+    use SoftDeletes, Translatable;
+
+    protected $fillable = ['property_id', 'name', 'name_en'];
+
+    public function property()
+    {
+        return $this->belongsTo(Property::class);
+    }
+
+    //TODO: check table name and fields
+    public function skus()
+    {
+        return $this->belongsToMany(Sku::class);
+    }
+}
