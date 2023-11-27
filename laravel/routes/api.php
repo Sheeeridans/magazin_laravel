7a3e1344315202d0e342fad9397219a100e878bd
sheridans <asy201196@yandex.ru>
2023-11-27 21:16:56 +0300

    less 41 Auth Guard & Providers

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,8 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('skus', 'Api\SkusController@getSkus');
 });
 
-Route::get('skus', 'Api\SkusController@getSkus');
