a873dd51d4b42029055cc2125d82eeea8033965d
sheridans <asy201196@yandex.ru>
2024-01-05 10:32:55 +0300

    fix products

--- laravel/resources/views/product.blade.php
+++ laravel/resources/views/product.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.master')
+
 @section('title', __('main.product'))
+
 @section('content')
     <h1>{{ $sku->product->__('name') }}</h1>
     <h2>{{ $sku->product->category->name }}</h2>
     <p>@lang('product.price'): <b>{{ $sku->price }} {{ $currencySymbol }}</b></p>
+
     @isset($sku->product->properties)
         @foreach ($sku->propertyOptions as $propertyOption)
             <h4>{{ $propertyOption->property->__('name') }}: {{ $propertyOption->__('name') }}</h4>
@@ -16,20 +19,23 @@
     @if($sku->isAvailable())
         <form action="{{ route('basket-add', $sku->product) }}" method="POST">
             <button type="submit" class="btn btn-success" role="button">@lang('product.add_to_cart')</button>
-            @else
 
-                <span>@lang('product.not_available')</span>
-                <br>
-                <span>@lang('product.tell_me'):</span>
-                <div class="warning">
-                    @if($errors->get('email'))
-                        {!! $errors->get('email')[0] !!}
-                    @endif
-                </div>
-                <form method="POST" action="{{ route('subscription', $sku) }}">
-                    @csrf
-                    <input type="text" name="email"></input>
-                    <button type="submit">@lang('product.subscribe')</button>
-                </form>
+            @csrf
+        </form>
+    @else
+
+        <span>@lang('product.not_available')</span>
+        <br>
+        <span>@lang('product.tell_me'):</span>
+        <div class="warning">
+            @if($errors->get('email'))
+                {!! $errors->get('email')[0] !!}
+            @endif
+        </div>
+        <form method="POST" action="{{ route('subscription', $sku) }}">
+            @csrf
+            <input type="text" name="email"></input>
+            <button type="submit">@lang('product.subscribe')</button>
+        </form>
     @endif
 @endsection
